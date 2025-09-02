229d14eaba48536a055a5f5526235f02048d5cf5
anselme <amotchojr@gmail.com>
2025-09-01 22:00:56 -0400

    Reapply "Fix service worker registration and update cache URLs to use relative paths"
    This reverts commit b0bd91dbecd229ea97463cd6d8bd6e69375981e3.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 // --- Service Worker Registration ---
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('./service-worker.js')
             .then(reg => console.log('ServiceWorker registration successful.'))
             .catch(err => console.log('ServiceWorker registration failed: ', err));
     });
